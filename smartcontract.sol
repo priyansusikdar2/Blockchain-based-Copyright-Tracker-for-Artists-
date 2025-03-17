8067a6679b2d7b1204a0229dbadcfed5ba40d587
priyansusikdar2 <priyansusikdar36@gmail.com>
2025-03-17 15:53:30 +0530

    Update smartcontract.sol

--- smartcontract.sol
+++ smartcontract.sol
@@ -1,1 +1,32 @@
+pragma solidity ^0.8.9;
+
+contract CopyrightTracker {
+    struct Artwork {
+        string title;
+        string artist;
+        string metadata;
+        bool registered;
+    }
+
+    mapping(uint256 => Artwork) public artworks;
+    uint256 public artworkCount;
+
+    // Register an artwork
+    function registerArtwork(string memory _title, string memory _artist, string memory _metadata) public {
+        artworkCount++;
+        artworks[artworkCount] = Artwork(_title, _artist, _metadata, true);
+    }
+
+    // Get artwork details by ID
+    function getArtwork(uint256 _id) public view returns (string memory, string memory, string memory, bool) {
+        require(_id > 0 && _id <= artworkCount, "Artwork does not exist");
+        Artwork memory art = artworks[_id];
+        return (art.title, art.artist, art.metadata, art.registered);
+    }
+
+    // Get the total number of registered artworks
+    function getArtworkCount() public view returns (uint256) {
+        return artworkCount;
+    }
+}
 
